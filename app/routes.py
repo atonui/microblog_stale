599231b6db8bb4c3b2fcdc5f9a0cc40ec2039650
Allan <a_tonui@yahoo.com>
2019-09-27 18:53:54 +0300

    Added profile editing capabilities

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,23 @@
 from app import app, db
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, EditProfileForm
 from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-      
+
     posts = [
         {
             'author': {'username':'John'},
@@ -76,3 +83,18 @@ def user(username):
         {'author':user,'body':'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
